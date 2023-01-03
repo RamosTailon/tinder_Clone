d2665032803712a11b2a8ae9133a3db71d6ea7df
Tailon Ramos <tailonramos11@gmail.com>
2023-01-03 17:51:14 -0300

    Função de registrar no BD e atualizado o readme

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # tinder_Clone
-um clone do tinder como desafio da Comunidade CoDe
+Um clone do tinder como desafio da Comunidade CoDe!
+A ideia do projeto é documentar minha forma de _codar_. Escolhi um projeto que tenha backend e frontend.
+
+## PWA com troca de telefones de usuários por matchs.
+
+![Preview do App](./preview_app.png)
+
+O projeto consiste em aplicação backend com Node.js, Banco de dados com MongoDb, e frontend com React.js
+
+as TECNOLOGIAS usadas foram:
+
+## Backend
+* bcrypt 
+* cookie-parser 
+* cors 
+* **express**
+* jsonwebtoken 
+* **mongoose**
+* multer 
+* nodemon
+
+## Frontend
+* **React.js**
+* React Router V6
+* yarn(vite)
+* styled-components //css escopado
+
+--------------------------------------------------------------
+Lista de tarefas:
+## Backend
+- [ ] Criar conexão com banco de dados
+- [ ] Criar Arquitetura MVC
+- [ ] Inicializar app com index.js
+- [ ] Criar Rotas e Controles
+- [ ] Fazer validações de usuário
+- [ ] Criar sistema de autenticação por tokens
+- [ ] Criar Login e verificação por token
+- [ ] Criar sistema de uploads de imagens
+- [ ] Criar regras de negócio da troca de telefone por Match
+
+## Frontend
+- [ ] Criar arquiteturas e rotas
+- [ ] Criar hooks de formulários e autenticação
+- [ ] Criar contexto de usuário
+- [ ] Criar Flash Message
+- [ ] Função de Login e Logout
+- [ ] Criar Sistema de atualização e preview de imagem
+- [ ] Dashboard de todos os usuários
+- [ ] Páginas de detalhes de próprio usuário
+- [ ] Páginas edição de usuário
+- [ ] Páginas de detalhes de outros usuários
+- [ ] Dashboard de match 
--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,12 +1,106 @@
 //IMPORTS
 const User = require('../models/User') // MODEL
 
+const bcrypt = require('bcrypt') // criptografa a senha e gera um token 
+
+const jwt = require('jsonwebtoken') // lê o token
+
 //HELPERS
 
 //START CLASS
 module.exports = class UserController {
 	static async register(req, res) {
-		res.json('teste 2')
+		const { name, email, password, confirmpassword, age, phone, images, bio, city, nation, interest } = req.body
+
+		if (!name) {
+			res.status(422).json({ message: "O nome é obrigatório!" })
+			return
+		}
+		if (!email) {
+			res.status(422).json({ message: "O e-mail é obrigatório" })
+			return
+		}
+		if (!password) {
+			res.status(422).json({ message: "A senha é obrigatória" })
+			return
+		}
+		if (!confirmpassword) {
+			res.status(422).json({ message: "A confirmação de senha é obrigatória" })
+			return
+		}
+
+		if (password !== confirmpassword) {
+			res.status(422).json({ message: "A senha e confirmação de senha precisam ser iguais" })
+			return
+		}
+
+		if (!age) {
+			res.status(422).json({ message: "A idade é obrigatória" })
+			return
+		}
+		if (!phone) {
+			res.status(422).json({ message: "O telefone é obrigatório" })
+			return
+		}
+		if (!images) {
+			res.status(422).json({ message: "Você deve adicionar ao menos uma foto" })
+			return
+		}
+		if (!bio) {
+			res.status(422).json({ message: "Você deve adicionar uma descrição na bio" })
+			return
+		}
+		if (!city) {
+			res.status(422).json({ message: "A cidade é obrigatória" })
+			return
+		}
+		if (!nation) {
+			res.status(422).json({ message: "O estado é obrigatório" })
+			return
+		}
+
+		//VERIFICAÇÃO SE O USUÁRIO EXISTE
+		const userExists = await User.findOne({ email: email }) //se esse usuário for preenchido é porque tem o mesmo email
+
+		if (userExists) {
+			res.status(422).json({ message: "Por favor, utilize outro email" })
+			return
+		}
+
+		//CRIAR A SENHA
+		const salt = await bcrypt.genSalt(12)
+
+		const passwordHash = await bcrypt.hash(password, salt) //super senha
+
+		//CRIAR O USUÁRIO
+		const user = new User({
+			name: name,
+			email: email,
+			password: passwordHash,
+			phone: phone,
+			age: age,
+			images: images,
+			bio: bio,
+			city: city,
+			nation: nation,
+			interest: interest
+		})
+
+		try {
+			//.save MÉTODO DO MONGOOSE
+			const newUser = await user.save()
+			res.status(201).json({
+				message: "usuário criado!",
+				newUser
+			})
+			// await createUserToken(newUser, req, res)
+
+		} catch (err) {
+			res.status(500).json({ message: err })
+		}
+
+
+
 
 	}
 
